72feeac63710a9b5687cea10896d33324574eaf2
4260snow <75683039+4260snow@users.noreply.github.com>
2023-10-11 00:59:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,26 +2,6 @@
 #include <string.h>
 
 void TrimRight(char* s){
-    /*char *s2 = s;
-    int counter = 0;
-    while (*s2 == ' '){
-        s2++;
-        counter++;
-    }
-    
-    char* right = s2;
-    while(*s2){
-        if(*s2 != ' ')
-            right = s2 + 1;
-        s2++;
-    }        
-    *right = 0;
-    
-    while(*(s + counter) != 0){
-        *s = *(s + counter);
-        s++;
-    }
-    *s = 0;*/
     char* right = s;
     while(*s){
         if(*s != ' ')
